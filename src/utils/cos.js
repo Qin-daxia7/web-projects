754c89c2c1697da682bc1b6e19b8d66a0bd6eca5
Qindaxia <qindaxia7@gmail.com>
2025-05-12 17:38:26 +0800

    second commit

--- src/utils/cos.js
+++ src/utils/cos.js
@@ -1,6 +1,6 @@
 import COS from "cos-js-sdk-v5";
 const cos = new COS({
-  SecretId: "AKIDFMHOXTChz4vqcDesthUBoERSxGPK4dz0",
-  SecretKey: "we9SYTSSLRRF0e7O3av2ucGhutRoy8O4",
+  SecretId: "test-AKIDFMHOXTChz4vqcDesthUBoERSxGPK4dz0",
+  SecretKey: "test-we9SYTSSLRRF0e7O3av2ucGhutRoy8O4",
 });
 export default cos;
